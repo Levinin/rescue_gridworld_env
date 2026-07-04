989b93446ae3f50740d93e1ff8630d63a79e736d
Andy Edmondson <andrew.edmondson@gmail.com>
2026-07-04 10:54:32 +0100

    Change room update method

--- rescue_gridworld/__init__.py
+++ rescue_gridworld/__init__.py
@@ -5,7 +5,7 @@ from .create_rooms import create_room_data_grid, ensure_capacity
 
 # Register the environment
 register(
-    id="RescueGridworld-v0",
+    id="RescueGridworld-v1",
     entry_point="rescue_gridworld.env:RescueGridworldEnv",
 )
 
